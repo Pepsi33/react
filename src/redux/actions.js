b5b09ded3f3b599dc4dd193819475830f5db6c86
Pepsi33 <319853035@qq.com>
2018-03-18 13:59:45 +0800

    修改

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,30 +1,24 @@
-
 import * as type from './constants/ActionTypes';
 
-export const login = (data,id) => ({
-  type: type.USER_LOGIN,
-  data,
-  id
+export const login = (data) => ({
+    type: type.USER_LOGIN,
+    data
 });
 
 export const layout = () => ({
-  type: type.USER_LAYOUT
+    type: type.USER_LAYOUT
 });
 
 
 
 
 //报表参数数据
-export const setRptParamsData = (data,id) => ({
-  type: type.RPT_PAM_DATA_SET,
-  data,
-  id
+export const setRptParamsData = (data, id) => ({
+    type: type.RPT_PAM_DATA_SET,
+    data,
+    id
 });
 
 export const getRptParamsData = () => ({
-  type: type.RPT_PAM_DATA_GET
-}); 
-
-
-
-
+    type: type.RPT_PAM_DATA_GET
+});
